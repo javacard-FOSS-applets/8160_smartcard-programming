efce399d4f60be68f6ec0f0afdd23184faa83b4a
prayzzz <prayzzz@outlook.com>
2015-05-13 15:09:52 +0200

    - added hotelbuddy

---
+++ hotelbuddy/src/main/java/hotelbuddy.java
@@ -0,0 +1,13 @@
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISOException;
+
+/**
+ * Created by Patrick on 13.05.2015.
+ */
+public class hotelbuddy extends Applet
+{
+    public void process(APDU apdu) throws ISOException
+    {
+    }
+}
---
+++ hotelbuddy/src/test/java/hotelbuddyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Patrick on 13.05.2015.
+ */
+public class hotelbuddyTest
+{
+    @Test
+    public void Test()
+    {
+    }
+}
