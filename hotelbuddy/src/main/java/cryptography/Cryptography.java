451b003a07ad3a5130e1748f08016251529b40fc
ZickZakk <gjenschmischek@gmail.com>
2015-06-19 12:24:28 +0200

    - renamed AID to starting with Capital Letter - added empty name check on card, when getName is called - added test stub for empty name check

--- hotelbuddy/src/main/java/cryptography/Cryptography.java
+++ hotelbuddy/src/main/java/cryptography/Cryptography.java
@@ -14,8 +14,8 @@ public class Cryptography extends Applet implements ICryptography
     private static final byte CRYPTOGRAPHY_SECRET = 0x2A;
 
     // Clients
-    private static final byte[] IDENTIFICATION_AID = {0x69, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e};
-    private static final byte[] CRYPTOGRAPHY_AID = {0x63, 0x72, 0x79, 0x70, 0x74, 0x6f, 0x67, 0x72, 0x61, 0x70, 0x68, 0x79};
+    private static final byte[] IDENTIFICATION_AID = {0x49, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e};
+    private static final byte[] CRYPTOGRAPHY_AID = {0x43, 0x72, 0x79, 0x70, 0x74, 0x6f, 0x67, 0x72, 0x61, 0x70, 0x68, 0x79};
 
     // Instructions
     private final static byte EXPORT_RSA_PUBLIC_MOD = (byte) 0xF0;
@@ -151,7 +151,6 @@ public class Cryptography extends Applet implements ICryptography
         return response;
     }
 
-    @Override
     public Shareable getShareableInterfaceObject(AID client_aid, byte parameter)
     {
         if (!client_aid.equals(IDENTIFICATION_AID, (short) 0, (byte) IDENTIFICATION_AID.length))
