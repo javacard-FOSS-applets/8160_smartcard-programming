3b6ec81eb54503db9c9f772508da9bef81bdce5c
ZickZakk <gjenschmischek@gmail.com>
2015-06-24 12:47:24 +0200

    - added getAge and checkAge - added Tests for getAge and checkAge - added DateHelper-Class for Oncard date handling (must be proven that this works in JCOP)

--- hotelbuddy/src/test/java/hotelbuddy/IdentificationTest.java
+++ hotelbuddy/src/test/java/hotelbuddy/IdentificationTest.java
@@ -3,7 +3,6 @@ package hotelbuddy;
 import com.licel.jcardsim.base.Simulator;
 import common.CryptographyMock;
 import common.TestHelper;
-import hotelbuddy.Identification;
 import javacard.framework.AID;
 import org.junit.Assert;
 import org.junit.Test;
@@ -109,6 +108,38 @@ public class IdentificationTest
     }
 
     @Test
+    public void Test_SetName_Second_Time_Throws_WRONG_LENGTH()
+    {
+        String name = "asd";
+
+        Simulator sim = new Simulator();
+
+        sim.installApplet(CryptographyAID, CryptographyMock.class);
+        sim.installApplet(IdentificationAID, Identification.class);
+
+        System.out.println("Getting ATR...");
+        byte[] atr = sim.getATR();
+        System.out.println(new String(atr));
+        System.out.println(TestHelper.ToHexString(atr));
+
+        System.out.println("\nSelecting Applet...");
+        CryptographyMock.DataLength = (short) name.length();
+        boolean isAppletSelected = sim.selectApplet(IdentificationAID);
+        System.out.println(isAppletSelected);
+
+        byte[] answer;
+        System.out.println("\nSetting Name");
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xA0, name.getBytes(), (byte) 0x00);
+        TestHelper.EnsureStatusBytesNoError(answer);
+
+        System.out.println("\nSetting Name Second Time");
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xA0, name.getBytes(), (byte) 0x00);
+        TestHelper.EnsureStatusBytes(answer, new byte[]{(byte) 0x69, (byte) 0x86});
+
+        CryptographyMock.reset();
+    }
+
+    @Test
     public void Test_SetName_Too_Short_Throws_WRONG_LENGTH()
     {
         String name = "";
@@ -138,7 +169,7 @@ public class IdentificationTest
     }
 
     @Test
-    public void Test_SetBirthday()
+    public void Test_Birthday()
     {
         byte[] birthday = {(byte) 12, (byte) 10, (byte) 19, (byte) 92};
 
@@ -156,18 +187,26 @@ public class IdentificationTest
         boolean isAppletSelected = sim.selectApplet(IdentificationAID);
         System.out.println(isAppletSelected);
 
+        CryptographyMock.DataLength = (short) birthday.length;
+
         byte[] answer;
         System.out.println("\nSetting Birthday");
-        CryptographyMock.DataLength = (short) birthday.length;
         answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB0, birthday, (byte) 0x00);
         TestHelper.EnsureStatusBytesNoError(answer);
         Assert.assertTrue(CryptographyMock.decryptWasCalled());
+
+        System.out.println("\nGetting Birthday");
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB1, birthday, (byte) 0x00);
+        TestHelper.EnsureStatusBytesNoError(answer);
+        Assert.assertTrue(CryptographyMock.encryptWasCalled());
+
+        TestHelper.compareWithoutStatusBytes(birthday, answer, birthday.length);
 
         CryptographyMock.reset();
     }
 
     @Test
-    public void Test_SetBirthday_Throws_WRONG_LENGTH()
+    public void Test_SetBirthday_With_Wrong_Date_Lengths_Throws_WRONG_LENGTH()
     {
         byte[] tooShortBirthday = {(byte) 12, (byte) 10, (byte) 19};
         byte[] tooLongBirthday = {(byte) 12, (byte) 10, (byte) 19, (byte) 19, (byte) 19};
@@ -203,7 +242,7 @@ public class IdentificationTest
     }
 
     @Test
-    public void Test_SetBirthday_Throws_DATA_INVALID()
+    public void Test_SetBirthday_With_Invalid_Dates_Throws_DATA_INVALID()
     {
         ArrayList<byte[]> wrongBirthdays = new ArrayList<>();
         wrongBirthdays.add(new byte[]{(byte) 0, (byte) 10, (byte) 19, (byte) 92});
@@ -232,7 +271,7 @@ public class IdentificationTest
         for (byte[] message : wrongBirthdays)
         {
             byte[] answer;
-            System.out.println("\nSetting Too Wrong Birthday");
+            System.out.println("\nSetting Too Invalid Birthday");
             CryptographyMock.DataLength = (short) message.length;
             answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB0, message, (byte) 0x00);
             TestHelper.EnsureStatusBytes(answer, new byte[]{(byte) 0x69, (byte) 0x84});
@@ -240,4 +279,173 @@ public class IdentificationTest
             CryptographyMock.reset();
         }
     }
+
+    @Test
+    public void Test_SetBirthday_Second_Time_Throws_WRONG_LENGTH()
+    {
+        byte[] birthday = {(byte) 12, (byte) 10, (byte) 19, (byte) 92};
+
+        Simulator sim = new Simulator();
+
+        sim.installApplet(CryptographyAID, CryptographyMock.class);
+        sim.installApplet(IdentificationAID, Identification.class);
+
+        System.out.println("Getting ATR...");
+        byte[] atr = sim.getATR();
+        System.out.println(new String(atr));
+        System.out.println(TestHelper.ToHexString(atr));
+
+        System.out.println("\nSelecting Applet...");
+        CryptographyMock.DataLength = (short) birthday.length;
+        boolean isAppletSelected = sim.selectApplet(IdentificationAID);
+        System.out.println(isAppletSelected);
+
+        byte[] answer;
+        System.out.println("\nSetting Birthday");
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xA0, birthday, (byte) 0x00);
+        TestHelper.EnsureStatusBytesNoError(answer);
+
+        System.out.println("\nSetting Birthday Second Time");
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xA0, birthday, (byte) 0x00);
+        TestHelper.EnsureStatusBytes(answer, new byte[]{(byte) 0x69, (byte) 0x86});
+
+        CryptographyMock.reset();
+    }
+
+    @Test
+    public void Test_GetBirthday_Before_SetBirthday_Throws_COMMAND_NOT_ALLOWED()
+    {
+        Simulator sim = new Simulator();
+
+        sim.installApplet(CryptographyAID, CryptographyMock.class);
+        sim.installApplet(IdentificationAID, Identification.class);
+
+        System.out.println("Getting ATR...");
+        byte[] atr = sim.getATR();
+        System.out.println(new String(atr));
+        System.out.println(TestHelper.ToHexString(atr));
+
+        System.out.println("\nSelecting Applet...");
+        boolean isAppletSelected = sim.selectApplet(IdentificationAID);
+        System.out.println(isAppletSelected);
+
+        byte[] answer;
+        System.out.println("\nTry getting Birthday, before setting Birthday");
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB1, new byte[0], (byte) 0x00);
+        TestHelper.EnsureStatusBytes(answer, new byte[]{(byte) 0x69, (byte) 0x86});
+        Assert.assertFalse(CryptographyMock.encryptWasCalled());
+        CryptographyMock.reset();
+    }
+
+    @Test
+    public void Test_CheckAge_Allowed()
+    {
+        byte[] birthday = {(byte) 12, (byte) 10, (byte) 19, (byte) 92};
+
+        ArrayList<byte[]> allowed = new ArrayList<>();
+
+        allowed.add(new byte[]{(byte) 11, (byte) 10, (byte) 19, (byte) 99, (byte) 6});
+        allowed.add(new byte[]{(byte) 12, (byte) 10, (byte) 19, (byte) 99, (byte) 7});
+        allowed.add(new byte[]{(byte) 13, (byte) 10, (byte) 19, (byte) 99, (byte) 7});
+        allowed.add(new byte[]{(byte) 12, (byte) 9, (byte) 19, (byte) 99, (byte) 6});
+        allowed.add(new byte[]{(byte) 12, (byte) 11, (byte) 19, (byte) 99, (byte) 7});
+
+        allowed.add(new byte[]{(byte) 11, (byte) 10, (byte) 20, (byte) 00, (byte) 7});
+        allowed.add(new byte[]{(byte) 12, (byte) 10, (byte) 20, (byte) 00, (byte) 8});
+        allowed.add(new byte[]{(byte) 13, (byte) 10, (byte) 20, (byte) 00, (byte) 8});
+        allowed.add(new byte[]{(byte) 12, (byte) 9, (byte) 20, (byte) 00, (byte) 7});
+        allowed.add(new byte[]{(byte) 12, (byte) 11, (byte) 20, (byte) 00, (byte) 8});
+
+        Simulator sim = new Simulator();
+
+        sim.installApplet(CryptographyAID, CryptographyMock.class);
+        sim.installApplet(IdentificationAID, Identification.class);
+
+        System.out.println("Getting ATR...");
+        byte[] atr = sim.getATR();
+        System.out.println(new String(atr));
+        System.out.println(TestHelper.ToHexString(atr));
+
+        System.out.println("\nSelecting Applet...");
+        boolean isAppletSelected = sim.selectApplet(IdentificationAID);
+        System.out.println(isAppletSelected);
+
+        byte[] answer;
+        System.out.println("\nSetting Birthday");
+        CryptographyMock.DataLength = (short) birthday.length;
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB0, birthday, (byte) 0x00);
+        TestHelper.EnsureStatusBytesNoError(answer);
+        CryptographyMock.reset();
+
+        for (byte[] message : allowed)
+        {
+            System.out.println("\nChecking Age Allowed");
+            CryptographyMock.DataLength = (short) message.length;
+            answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB2, message, (byte) 0x00);
+
+            TestHelper.EnsureStatusBytesNoError(answer);
+            Assert.assertTrue(CryptographyMock.decryptWasCalled());
+            Assert.assertTrue(CryptographyMock.encryptWasCalled());
+            TestHelper.compareWithoutStatusBytes(new byte[]{0x01}, answer, 1);
+
+            CryptographyMock.reset();
+        }
+    }
+
+    @Test
+    public void Test_CheckAge_Not_Allowed()
+    {
+        byte[] birthday = {(byte) 12, (byte) 10, (byte) 19, (byte) 92};
+
+        ArrayList<byte[]> notAllowed = new ArrayList<>();
+
+        notAllowed.add(new byte[]{(byte) 11, (byte) 10, (byte) 19, (byte) 99, (byte) 7});
+        notAllowed.add(new byte[]{(byte) 12, (byte) 10, (byte) 19, (byte) 99, (byte) 8});
+        notAllowed.add(new byte[]{(byte) 13, (byte) 10, (byte) 19, (byte) 99, (byte) 8});
+        notAllowed.add(new byte[]{(byte) 12, (byte) 9, (byte) 19, (byte) 99, (byte) 7});
+        notAllowed.add(new byte[]{(byte) 12, (byte) 11, (byte) 19, (byte) 99, (byte) 8});
+
+        notAllowed.add(new byte[]{(byte) 11, (byte) 10, (byte) 20, (byte) 00, (byte) 8});
+        notAllowed.add(new byte[]{(byte) 12, (byte) 10, (byte) 20, (byte) 00, (byte) 9});
+        notAllowed.add(new byte[]{(byte) 13, (byte) 10, (byte) 20, (byte) 00, (byte) 9});
+        notAllowed.add(new byte[]{(byte) 12, (byte) 9, (byte) 20, (byte) 00, (byte) 8});
+        notAllowed.add(new byte[]{(byte) 12, (byte) 11, (byte) 20, (byte) 00, (byte) 9});
+
+        Simulator sim = new Simulator();
+
+        sim.installApplet(CryptographyAID, CryptographyMock.class);
+        sim.installApplet(IdentificationAID, Identification.class);
+
+        System.out.println("Getting ATR...");
+        byte[] atr = sim.getATR();
+        System.out.println(new String(atr));
+        System.out.println(TestHelper.ToHexString(atr));
+
+        System.out.println("\nSelecting Applet...");
+        boolean isAppletSelected = sim.selectApplet(IdentificationAID);
+        System.out.println(isAppletSelected);
+
+        byte[] answer;
+        System.out.println("\nSetting Birthday");
+        CryptographyMock.DataLength = (short) birthday.length;
+        answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB0, birthday, (byte) 0x00);
+        TestHelper.EnsureStatusBytesNoError(answer);
+        CryptographyMock.reset();
+
+        for (byte[] message : notAllowed)
+        {
+            System.out.println("\nChecking Age Allowed");
+            CryptographyMock.DataLength = (short) message.length;
+            answer = TestHelper.ExecuteCommand(sim, (byte) 0x49, (byte) 0xB2, message, (byte) 0x00);
+
+            TestHelper.EnsureStatusBytesNoError(answer);
+            Assert.assertTrue(CryptographyMock.decryptWasCalled());
+            Assert.assertTrue(CryptographyMock.encryptWasCalled());
+            TestHelper.compareWithoutStatusBytes(new byte[]{0x00}, answer, 1);
+
+            CryptographyMock.reset();
+        }
+    }
+
+
 }
